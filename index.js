296c42f1a13a705274a17f286ac16a69ad38c401
Mesut Yılmaz <mesut.yilmaz@yga.org.tr>
2019-11-23 18:23:20 +0300

    Finished v1

--- index.js
+++ index.js
@@ -1,67 +1,122 @@
-const XLSX = require('xlsx');
-const fs = require('fs');
+/*
+Requirements
+*/
+let XLSX = require('xlsx');
+let fs = require('fs');
 
-const table = XLSX.readFile('WeWALK App Lokalizasyon.xlsx');
-const sheet = table.Sheets[table.SheetNames[0]];
-const range = XLSX.utils.decode_range(sheet['!ref']);
-
-const isiOS = true;
-
-const iOSFileNames = [
-  'Turkish',
-  'English',
-  'Portuguese',
-  'Arabic'
-];
-
-const androidFileNames = [
-  'values',
-  'values-tr',
-  'values-pt',
-  'values-ar'
-];
+// Replace All Method
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 
 
+/*
+Init
+*/
+let table = XLSX.readFile('lokalizasyon.xlsx');
 
-runScript();
+let sheet = table.Sheets[table.SheetNames[0]]; // First Sheet: Localisations
+let list = XLSX.utils.sheet_to_json(sheet);
 
-function runScript() {
-  let keyColumnNumber = 0;
-  if (isiOS) {
-    keyColumnNumber = 1;
-  }
+let parameterSheet = table.Sheets[table.SheetNames[1]]; // Second Sheet: Parameters
+let parameters = XLSX.utils.sheet_to_json(parameterSheet)[0];
 
-  for (let colNum = 2; colNum <= range.e.c; colNum++) {
-    let data = "";
-    for (let rowNum = range.s.r + 1; rowNum <= range.e.r; rowNum++) {
-      const keyCell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: keyColumnNumber })];
-      const cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: colNum })];
-      if (keyCell === undefined || cell === undefined) {  // for empty cells
-        continue;
-      }
-      console.log(cell.v);
-      if (isiOS) {
-        data += `"${keyCell.v}" = "${cell.v}";\n`
-      } else {
+let keys = list.shift(); // Retuns language names
+const platforms = ["android", "ios", "voice_menu"];
 
-      }
+let langCodes = Object.keys(keys).splice(4); // Why 4: #, android, ios, voice_menu
+let resource = { android: {}, ios: {}, voice_menu: {}};
+
+for (var i in langCodes) {
+    let langCode = langCodes[i];
+    // Adding language arrays to main resource
+    resource.android[langCode] = "";
+    resource.ios[langCode] = "";
+    resource.voice_menu[langCode] = "";
+}
+
+
+/*
+Iterating XLSX File
+*/
+for (var i in list) {
+    let row = list[i];
+    for (var j in langCodes) {
+
+        let langCode = langCodes[j];
+        if (!!row.voice_menu_key) resource.voice_menu[langCode] += processText("voice_menu", langCode, row.voice_menu_key, row[langCode]);
+        if (!!row.android_key) resource.android[langCode] += processText("android", langCode, row.android_key, row[langCode]);
+        if (!!row.ios_key) resource.ios[langCode] += processText("ios", langCode, row.ios_key, row[langCode]);
     }
-    if (isiOS) {
-      writeToFileForiOS(iOSFileNames[colNum - 2], data);
-    } else {
-      writeToFileForAndroid(androidFileNames[0], data);
+}
+
+
+/*
+Text Helper Funtion
+*/
+function processText(platform, lang, key, value) {
+    switch (platform) {
+        case "android":
+                value = String(value).replaceAll('"', String.fromCharCode(92) + '"');
+                value = value.replaceAll('{{0}}', '%d');
+                value = value.replaceAll('{{A}}', '%s');
+            return `<string name="${key}">${value}</string>\n`;
+        case "ios":
+            value = String(value).replaceAll('"', String.fromCharCode(92) + '"');
+            value = value.replaceAll('{{0}}', '%d');
+            value = value.replaceAll('{{A}}', '%@');
+            return `"${key}" = "${value}"\n`;
+        case "voice_menu":
+            return `say -v ${parameters[lang+"_voice_name"]} ${value} -o ${lang}_${key}.aiff\n`;
+        default:
+            return;
     }
-  }
 }
 
-function writeToFileForiOS(fileName, data) {
-  fs.writeFile(`${fileName}.strings`, data, (err) => {
-    if (err) throw err;
-  });
+
+/*
+Writing Files
+*/
+writeToFile();
+function writeToFile() {
+    for (var i in platforms) {
+        let platform = platforms[i];
+        let voice_menu_data = "";
+
+        for (var j in langCodes) {
+            let lang = langCodes[j];
+            let data = resource[platform][lang];
+            
+            if (platform === "ios") {
+                fileName = keys[lang] + ".strings";
+            }
+            if (platform === "android") {
+                fileName = `strings-${lang}.xml`;
+                let start = '<?xml version="1.0" encoding="utf-8" standalone="no"?><resources>';
+                let close = '</resources>'
+                data = data + closeXml;
+            }
+            if (platform === "voice_menu") {
+                voice_menu_data += data;
+            }
+            fs.writeFile(fileName, data, (err) => {
+                if (err) throw err;
+            });
+        }
+        fs.writeFile("voice_menu.txt", voice_menu_data, (err) => {
+            if (err) throw err;
+        });
+    }
 }
 
-function writeToFileForAndroid(fileName, data) {
-  fs.writeFile(`${fileName}.xml`, data, (err) => {
-    if (err) throw err;
-  });
-}
+/*
+
+TODOS
+- download file
+
+NOTES
+- New line automatically comes as \n from XLSX
+*/
+
+
