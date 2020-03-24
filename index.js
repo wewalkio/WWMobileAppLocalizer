fbb48cfd22720ab17115121fc9b08565a37b12c0
Halil ÖZCAN <halil.ozcan@wewalk.io>
2020-03-24 17:16:24 +0300

    Change arabic language handle style

--- index.js
+++ index.js
@@ -97,11 +97,11 @@ function generateLocalization() {
             case "android":
                 if (lang === "ar"){
                     // This dirty hack will be solved as {{O-n}} & {{O}} replacement rather than {{0-n}} {{0}}
-                    value = value.replaceAll('\{\{(O-)([0-9])\}\}', 'd$$$2%');//{{O-n}}
-                    value = value.replaceAll('\{\{(0-)([0-9])\}\}', 'd$$$2%');//{{0-n}}
-                    value = value.replaceAll('\{\{(A-)([0-9])\}\}', 's$$$2%');//{{A-n}}
-                    value = value.replaceAll('\{\{(0)\}\}', 'd%'); //{{0}}
-                    value = value.replaceAll('\{\{(A)\}\}', 's%'); // {{A}}
+                    value = value.replaceAll('\{\{(O-)([0-9])\}\}', '%$2$d');//{{O-n}}
+                    value = value.replaceAll('\{\{(0-)([0-9])\}\}', '%$2$d');//{{0-n}}
+                    value = value.replaceAll('\{\{(A-)([0-9])\}\}', '%$2$s');//{{A-n}}
+                    value = value.replaceAll('\{\{(0)\}\}', '%d'); //{{0}}
+                    value = value.replaceAll('\{\{(A)\}\}', '%s'); // {{A}}
                 } else{
                     value = value.replaceAll('\{\{(0-)([0-9])\}\}', '%$2$d');//{{0-n}}
                     value = value.replaceAll('\{\{(A-)([0-9])\}\}', '%$2$s');//{{A-n}}
